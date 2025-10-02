49d2b8b1d6f1535ff0f3e4cfda2417e78752a440
Marcelo Wizenberg <marcelo_wz@hotmail.com>
2025-10-02 00:05:02 -0300

    feat: added 3 charts do homepage grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
+import AppBarChart from "@/components/AppBarChart";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="destructive" size="lg" className="roundend-full">
-        <CirclePlus />
-        Click me 
-      </Button>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
     </div>
   );
 }
